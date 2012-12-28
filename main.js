fffa6a500f28c59dfb4fbd7e1b1ce520591792c7
Jared Hanson <jaredhanson@gmail.com>
2012-12-27 17:35:49 -0800

    Export Hash.

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-define(['exports'],
-function(exports) {
+define(['exports',
+        './lib/hash'],
+function(exports, Hash) {
+
+  exports.algo = 'md5';
+
+  exports.createHash = function() {
+    return new Hash();
+  }
+  exports.Hash = Hash;
+
 });
--- test/md5.test.js
+++ test/md5.test.js
@@ -1,4 +1,27 @@
 define(['md5'],
-function(crypto) {  
+function(md5) {
+
+  describe('md5', function() {
+  
+    it('should export algo', function() {
+      expect(md5.algo).to.equal('md5');
+    });
+  
+    it('should export createHash', function() {
+      expect(md5.createHash).to.be.a('function');
+    });
+    
+    it('should export Hash', function() {
+      expect(md5.Hash).to.be.a('function');
+    });
+    
+    describe('.createHash()', function() {
+      it('should create instance of Hash', function() {
+        expect(md5.createHash()).to.be.an.instanceOf(md5.Hash);
+      });
+    });
+  
+  });
+
   return { name: "test.md5" }
 });
